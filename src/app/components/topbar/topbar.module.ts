4a32fac668f5c5db792932c5ed1bd891928843f5
Balasaheb thakare <balasaheb.thakare@enrichai.com>
2021-10-22 17:29:19 +0530

    created topbar module

---
+++ src/app/components/topbar/topbar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopbarComponent } from './topbar/topbar.component';
+
+
+
+@NgModule({
+  declarations: [TopbarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TopbarModule { }
