4dad6983da78cab5dbc294f1ac4cb0b420dde7a5
Balasaheb thakare <balasaheb.thakare@enrichai.com>
2021-10-23 03:15:14 +0530

    created card component

--- src/app/components/card/card.module.ts
+++ src/app/components/card/card.module.ts
@@ -8,6 +8,7 @@ import { CardComponent } from './card/card.component';
   declarations: [CardComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [CardComponent]
 })
 export class CardModule { }
