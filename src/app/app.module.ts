10194b86bc6c4fd865871b4cf8e132393c959fc5
Balasaheb thakare <balasaheb.thakare@enrichai.com>
2021-10-23 03:14:52 +0530

    added services and required dependencies. in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KanbanDashboardModule } from './Home/kanban-dashboard/kanban-dashboard.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddTasksComponent } from './Home/kanban-dashboard/add-tasks/add-tasks.component';
+import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    AddTasksComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    KanbanDashboardModule
-
+    KanbanDashboardModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AddTasksComponent]
 })
 export class AppModule { }
