2b6c2cb9425b995502ef7a5c834c12a7300cfbcd
Balasaheb thakare <balasaheb.thakare@enrichai.com>
2021-10-22 17:25:10 +0530

    created kanban module

---
+++ src/app/Home/kanban-dashboard/kanban-dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KanbanDashboardComponent } from './kanban-dashboard/kanban-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [KanbanDashboardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class KanbanDashboardModule { }
